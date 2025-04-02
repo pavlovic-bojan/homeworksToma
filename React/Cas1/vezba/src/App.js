5c486efa0f37e9a17c60f7b09a7d9a5246dff23e
Bojan Pavlovic <bojanpavlovicoffice@gmail.com>
2025-04-02 09:37:07 +0200

    finish first vezba

--- React/Cas1/vezba/src/App.js
+++ React/Cas1/vezba/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let ime = "Bojan"
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <p>Pozdrav ja se zovem {ime}</p>
       </header>
     </div>
   );
