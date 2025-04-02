5ea51ad0f89a2e9698350a98e50407339f3c3fce
Bojan Pavlovic <bojanpavlovicoffice@gmail.com>
2025-04-02 10:14:15 +0200

    finished first domaci

---
+++ React/Cas1/domaci/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let currentHour = new Date().getHours()
+  let divStyle = {
+    backgroundColor: currentHour >=20 && currentHour <= 7 ? '#10375C' : '#EB8317',
+    padding: '20px',
+    color: 'white',
+  }
+
+  return (
+      <div className="App">
+          <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo"/>
+              <p>
+                  Edit <code>src/App.js</code> and save to reload.
+              </p>
+              <a
+                  className="App-link"
+                  href="https://reactjs.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+              >
+                  Learn React
+              </a>
+              <p style={divStyle}>
+                  Ovo je neki div
+              </p>
+          </header>
+      </div>
+  );
+}
+
+export default App;
+
