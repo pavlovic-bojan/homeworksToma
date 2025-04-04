d81700eef5e55e0b4c837ebcc1f2a77f82888e8a
Bojan Pavlovic <bojanpavlovicoffice@gmail.com>
2025-04-04 20:09:49 +0200

    vezba2 cas3

--- React/Cas1/vezba/src/Components/Products.tsx
+++ React/Cas1/vezba/src/Components/Products.tsx
@@ -13,9 +13,18 @@ const products:ProductInterface = {
 
 function Products(props: { pdv: number; }) {
     let [pdv, setPdv] = useState(props.pdv)
+
+    let [productList, setProductList] = useState<ProductInterface>(products);
+
+
     function changePdv(e: { target: { value: number; }; }){
-        setPdv(e.target.value)
-        console.log(pdv);
+        setPdv(e.target.value);
+    }
+
+    function deleteProduct(productName: string) {
+        const updatedProducts = { ...productList };
+        delete updatedProducts[productName];
+        setProductList(updatedProducts);
     }
 
     return (
@@ -23,10 +32,11 @@ function Products(props: { pdv: number; }) {
             <ul>
                 <h1>Change Pdv tax</h1>
                 <input type="number" onInput={changePdv}/>
-                {Object.entries(products).map(([product, price], index) => (
+                {Object.entries(productList).map(([product, price]: [string, number], index) => (
                     <li key={index}>
                         {product} - cena bez pdv: ${price} + pdv {pdv}%
                         <br/>cena sa pdv: ${calculatePDV(price, pdv)}
+                        <button onClick={() => deleteProduct(product)}>Delete</button>
                     </li>
                 ))}
             </ul>
