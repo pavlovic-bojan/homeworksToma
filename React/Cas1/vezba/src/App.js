e9f07aecd607f8fd27445518337c7ea517e132e6
Bojan Pavlovic <bojanpavlovicoffice@gmail.com>
2025-04-05 05:02:47 +0200

    vezbe for cas4

--- React/Cas1/vezba/src/App.js
+++ React/Cas1/vezba/src/App.js
@@ -1,33 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import Products from './Components/Products';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Products from './Components/Products'
 
 function App() {
-  let ime = "Bojan"
-  let color = { color: 'yellow' }
   return (
       <>
         <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo"/>
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-            <p>Pozdrav ja se zovem {ime}</p>
-            <p style={color}>Ovo je moji prvi react app</p>
             <Products pdv = '20' />
-          </header>
         </div>
       </>
   );
 }
 
-export default App;
+export default App
--- React/Cas1/vezba/src/Components/Products.tsx
+++ React/Cas1/vezba/src/Components/Products.tsx
@@ -1,10 +1,10 @@
 import React, {useState} from "react";
 
-interface ProductInterface{
-    [key:string]:number
+interface ProductInterface {
+    [key: string]: number
 }
 
-const products:ProductInterface = {
+const products: ProductInterface = {
     'iPhone13': 1000,
     'iPhone14': 1100,
     'iPhone15': 1200,
@@ -12,105 +12,148 @@ const products:ProductInterface = {
 };
 
 function Products(props: { pdv: number; }) {
+    let [search, setSearch] = useState<string>("")
+    let [searchResult, setSearchResult] = useState<boolean | null>(null)
     let [pdv, setPdv] = useState(props.pdv)
-    let [productList, setProductList] = useState<ProductInterface>(products);
-    let [newProductName, setNewProductName] = useState<string>("");
-    let [newProductPrice, setNewProductPrice] = useState<number>(0);
-    let [editingProductName, setEditingProductName] = useState<string>("");
-    let [editingProductPrice, setEditingProductPrice] = useState<number>(0);
+    let [newProductName, setNewProductName] = useState("")
+    let [newProductPrice, setNewProductPrice] = useState<number>(0)
+    let [editingProductName, setEditingProductName] = useState<string | null>(null)
+    let [productList, setProductList] = useState<ProductInterface>(products)
 
-    function changePdv(e: { target: { value: number; }; }){
-        setPdv(e.target.value);
+    function updateProductList(callback: (products: ProductInterface) => ProductInterface) {
+        setProductList((prev) => callback({...prev}))
+    }
+
+    function searchField(e: React.ChangeEvent<HTMLInputElement>) {
+        search = e.currentTarget.value
+        setSearch(search)
+
+        const exists = Object.keys(productList).some(
+            (product) => product.toLowerCase() === search.toLowerCase()
+        )
+        setSearchResult(search ? exists : null)
+        console.log(exists)
+    }
+
+    function changePdv(e: React.ChangeEvent<HTMLInputElement>) {
+        pdv = e.currentTarget.value
+        setPdv(pdv)
     }
 
     function deleteProduct(productName: string) {
-        const updatedProducts = { ...productList };
-        delete updatedProducts[productName];
-        setProductList(updatedProducts);
+        updateProductList((products) => {
+            delete products[productName]
+            return products
+        })
     }
 
     function deleteAllProducts() {
-        setProductList({});
+        updateProductList(() => ({}))
     }
 
     function startEditingProduct(productName: string, productPrice: number) {
-        setEditingProductName(productName);
-        setEditingProductPrice(productPrice);
+        setNewProductName(productName)
+        setNewProductPrice(productPrice)
+        setEditingProductName(productName)
     }
 
-    function saveEditedProduct() {
-        if (editingProductName && editingProductPrice > 0) {
-            const updatedProducts = { ...productList, [editingProductName]: editingProductPrice };
-            setProductList(updatedProducts);
-            setEditingProductName("");
-            setEditingProductPrice(0);
-        } else {
-            alert("Please provide a valid product name and price.");
-        }
-    }
-
-    function addProduct() {
+    function saveProduct() {
         if (newProductName && newProductPrice > 0) {
-            const updatedProducts = { ...productList, [newProductName]: newProductPrice };
-            setProductList(updatedProducts);
-            setNewProductName("");
-            setNewProductPrice(0);
+            updateProductList((products) => {
+                if (editingProductName && editingProductName !== newProductName) {
+                    delete products[editingProductName]
+                }
+                products[newProductName] = newProductPrice
+                return products
+            })
+
+            setNewProductName("")
+            setNewProductPrice(0)
+            setEditingProductName(null)
         } else {
-            alert("Please provide a valid product name and price.");
+            alert("Please provide a valid product name and price.")
         }
     }
 
     return (
         <>
-        <ul>
-            <h2>Add New Product</h2>
-            <input
-                type="text"
-                placeholder="Product Name"
-                value={newProductName}
-                onInput={(e: { target: { value: string; }; }) => setNewProductName(e.target.value)}
-            />
-            <input
-                type="number"
-                placeholder="Product Price"
-                value={newProductPrice}
-                onInput={(e: { target: { value: number; }; }) => setNewProductPrice(Number(e.target.value))}
-            />
-            <button onClick={addProduct}>Add Product</button>
-            <br/>
-            <h2>Edit Product</h2>
-            <input
-                type="text"
-                value={editingProductName}
-                onInput={(e: { target: { value: string; }; }) => setEditingProductName(e.target.value)}
-            />
-            <input
-                type="number"
-                value={editingProductPrice}
-                onInput={(e: { target: { value: number; }; }) => setEditingProductPrice(Number(e.target.value))}
-            />
-            <button onClick={saveEditedProduct}>Save Changes</button>
-        <br/>
-        <h1>Change Pdv tax</h1>
-        <input type="number" placeholder="Change Pdv tax" onInput={changePdv}/>
-        <br/>
-        {Object.entries(productList).map(([product, price]: [string, number], index) => (
-            <li key={index}>
-                {product} - cena bez pdv: ${price} + pdv {pdv}%
-                <br/>cena sa pdv: ${calculatePDV(price, pdv)}
-                <button onClick={() => startEditingProduct(product, price)}>Edit</button>
-                <button onClick={() => deleteProduct(product)}>Delete</button>
-            </li>
-        ))}
-        </ul>
-    <button onClick={deleteAllProducts}>Delete All Products</button>
-</>
-)
-    ;
+            <div className="d-flex flex-column align-items-center">
+                <h1 className="mb-3">Add New Product Or Edit Existing Product</h1>
+
+                <div className="mb-3 w-50">
+                    <input
+                        type="text"
+                        className="form-control mb-3"
+                        placeholder="Product Name"
+                        value={newProductName}
+                        onChange={(e) => setNewProductName(e.target.value)}
+                    />
+                    <input
+                        type="number"
+                        className="form-control mb-3"
+                        placeholder="Product Price"
+                        value={newProductPrice}
+                        onChange={(e) => setNewProductPrice(Number(e.target.value))}
+                    />
+                    <button className="btn btn-success w-100" onClick={saveProduct}>
+                        Save Product
+                    </button>
+                    <hr/>
+                    <h2 className="mb-4">Change PDV tax</h2>
+                    <input
+                        type="number"
+                        className="form-control mb-4"
+                        placeholder="Change PDV tax"
+                        value={pdv}
+                        onChange={changePdv}
+                    />
+                    <hr/>
+                    <h2 className="mb-4">Search Product</h2>
+                    {searchResult !== null && (
+                        <div className={`alert ${searchResult ? "alert-success" : "alert-danger"}`} role="alert">
+                            {searchResult
+                                ? "You have successfully found the product."
+                                : "You did not find the product."}
+                        </div>
+                    )}
+                    <input
+                        type="text"
+                        className="form-control mb-3"
+                        placeholder="Insert key word for Search"
+                        value={search}
+                        onChange={searchField}
+                    />
+                    <hr/>
+                    <button className="btn btn-danger w-100" onClick={deleteAllProducts}>
+                        Delete All Products
+                    </button>
+                </div>
+                <div className="d-flex flex-wrap justify-content-center w-100">
+                    {Object.entries(productList).map(([product, price], index) => (
+                        <div key={index} className="card m-3 p-3" style={{width: "18rem"}}>
+                            <h5 className="card-title">{product}</h5>
+                            <p className="card-text">Cena bez pdv: ${price} + pdv {pdv}%</p>
+                            <p className="card-text">Cena sa pdv: ${calculatePDV(price as number, pdv)}</p>
+                            <div className="d-flex justify-content-between">
+                                <button className="btn btn-primary"
+                                        onClick={() => startEditingProduct(product, price as number)}>
+                                    Edit
+                                </button>
+                                <button className="btn btn-danger" onClick={() => deleteProduct(product)}>
+                                    Delete
+                                </button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+
+    )
 }
 
 function calculatePDV(price: number, pdv: number): number {
     return ((price * pdv) / 100) + price
 }
 
-export default Products;
+export default Products
