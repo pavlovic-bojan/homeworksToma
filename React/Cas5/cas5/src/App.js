2bcee97963e432dd64293e33e7491ae08430eab9
Bojan Pavlovic <bojanpavlovicoffice@gmail.com>
2025-04-06 09:00:42 +0200

    youtube app create video array

--- React/Cas5/cas5/src/App.js
+++ React/Cas5/cas5/src/App.js
@@ -1,6 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
+const VIDEOS = [
+    {
+      id: 1,
+      title: 'How To Learn React',
+      url: 'https://www.youtube.com/watch?v=SqcY0GlETPk&t=163s&ab_channel=ProgrammingwithMosh',
+      cover: 'https://www.youtube.com/watch?v=SqcY0GlETPk&t=163s&ab_channel=ProgrammingwithMosh',
+    },
+    {
+        id: 2,
+        title: 'How To Learn CSS',
+        url: 'https://www.youtube.com/watch?v=1PnVor36_40&ab_channel=WebDevSimplified',
+        cover: 'https://www.youtube.com/watch?v=1PnVor36_40&ab_channel=WebDevSimplified',
+    }
+]
+
 const App = () => {
   return (
     <>youtube</>
