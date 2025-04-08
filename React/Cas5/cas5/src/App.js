dfbf6c48c440bf584d84e16e28cc512bc2f37d8c
Bojan Pavlovic <bojanpavlovicoffice@gmail.com>
2025-04-08 05:24:16 +0200

    youtube app added first vezba fix

--- React/Cas5/cas5/src/App.js
+++ React/Cas5/cas5/src/App.js
@@ -18,29 +18,31 @@ const VIDEOS = [
 
 const App = () => {
     return (
-        <div className="container mt-4">
-            <h2 className="mb-4">YouTube Video List</h2>
-            <div className="row">
-                {VIDEOS.map(video => (
-                    <div className="col-md-3 mb-4" key={video.id}>
-                        <a href={video.url} target="_blank" rel="noopener noreferrer">
-                            <img
-                                src={video.cover}
-                                alt={video.title}
-                                width="400"
-                                height="370"
-                                className="img-fluid mb-2"
-                            />
-                        </a>
-                        <div>
+        <>
+            <div className="container mt-4">
+                <h2 className="mb-4">YouTube Video List</h2>
+                <div className="row">
+                    {VIDEOS.map(video => (
+                        <div className="col-md-3 mb-4" key={video.id}>
                             <a href={video.url} target="_blank" rel="noopener noreferrer">
-                                {video.title}
+                                <img
+                                    src={video.cover}
+                                    alt={video.title}
+                                    width="400"
+                                    height="370"
+                                    className="img-fluid mb-2"
+                                />
                             </a>
+                            <div>
+                                <a href={video.url} target="_blank" rel="noopener noreferrer">
+                                    {video.title}
+                                </a>
+                            </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
