e6906f5e51cf1d257354c635adf00f8b0f7eab49
Bojan Pavlovic <bojanpavlovicoffice@gmail.com>
2025-04-08 05:22:24 +0200

    youtube app added first vezba done

--- React/Cas5/cas5/src/App.js
+++ React/Cas5/cas5/src/App.js
@@ -3,10 +3,10 @@ import './App.css'
 
 const VIDEOS = [
     {
-      id: 1,
-      title: 'How To Learn React',
-      url: 'https://www.youtube.com/watch?v=SqcY0GlETPk&t=163s&ab_channel=ProgrammingwithMosh',
-      cover: 'https://i.ytimg.com/vi/SqcY0GlETPk/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLBvF7R7tYEZqgAYn6fM5A_QgI1e-A',
+        id: 1,
+        title: 'How To Learn React',
+        url: 'https://www.youtube.com/watch?v=SqcY0GlETPk&t=163s&ab_channel=ProgrammingwithMosh',
+        cover: 'https://i.ytimg.com/vi/SqcY0GlETPk/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLBvF7R7tYEZqgAYn6fM5A_QgI1e-A',
     },
     {
         id: 2,
@@ -17,9 +17,31 @@ const VIDEOS = [
 ]
 
 const App = () => {
-  return (
-    <>youtube</>
-  )
+    return (
+        <div className="container mt-4">
+            <h2 className="mb-4">YouTube Video List</h2>
+            <div className="row">
+                {VIDEOS.map(video => (
+                    <div className="col-md-3 mb-4" key={video.id}>
+                        <a href={video.url} target="_blank" rel="noopener noreferrer">
+                            <img
+                                src={video.cover}
+                                alt={video.title}
+                                width="400"
+                                height="370"
+                                className="img-fluid mb-2"
+                            />
+                        </a>
+                        <div>
+                            <a href={video.url} target="_blank" rel="noopener noreferrer">
+                                {video.title}
+                            </a>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
 }
 
 export default App
