fbd09a186e52b75037785b50264df3497c6c78f6
Bojan Pavlovic <bojanpavlovicoffice@gmail.com>
2025-04-09 08:02:39 +0200

    done vezba3 for cas 6

--- React/Cas5/cas5/src/Pages/Home.js
+++ React/Cas5/cas5/src/Pages/Home.js
@@ -1,7 +1,12 @@
 import AllVideos from "../Components/AllVideos"
-import SearchBar from "../Components/SearchBar";
+import SearchBar from "../Components/SearchBar"
+import VIDEOS from "../videos.json"
+import {useState} from "react";
 
 const Home = () => {
+
+    let [videos, setVideos] = useState(VIDEOS)
+
     return (
         <>
             <h1>Home page</h1>
